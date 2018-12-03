b91cf13cc4d2bf675fbf17ebcd04bdd5da35f8fb
Catouse <catouse@gmail.com>
2018-12-03 17:17:32 +0800

    * improve UI of chat title view.

--- xxc/app/views/chats/chat-title.js
+++ xxc/app/views/chats/chat-title.js
@@ -93,9 +93,10 @@ export default class ChatTitle extends Component {
             ...other
         } = this.props;
 
+        const denyShowMemberProfile = Config.ui['chat.denyShowMemberProfile'];
         const chatName = chat.getDisplayName(App, true);
         const theOtherOne = chat.isOne2One ? chat.getTheOtherOne(App) : null;
-        const onTitleClick = (!Config.ui['chat.denyShowMemberProfile'] && theOtherOne) ? MemberProfileDialog.show.bind(null, theOtherOne, null) : null;
+        const onTitleClick = (!denyShowMemberProfile && theOtherOne) ? MemberProfileDialog.show.bind(null, theOtherOne, null) : null;
         this.lastOtherOneUpdateId = theOtherOne && theOtherOne.updateId;
         this.lastChatUpdateId = chat.updateId;
 
@@ -115,8 +116,9 @@ export default class ChatTitle extends Component {
             {hideChatAvatar ? null : <ChatAvatar chat={chat} size={24} className={theOtherOne ? 'state' : ''} onClick={onTitleClick} />}
             {showStatusDot && <StatusDot status={theOtherOne.status} />}
             {
-                theOtherOne ? <a className="strong rounded title flex-none text-primary" onClick={onTitleClick}>{chatName}</a> : <strong className="title flex-none">{chatName}</strong>
+                (!denyShowMemberProfile && theOtherOne) ? <a className="strong rounded title flex-none text-primary" onClick={onTitleClick}>{chatName}</a> : <strong className="title flex-none">{chatName}</strong>
             }
+            {(theOtherOne && !showStatusDot) ? <span className="muted">[{Lang.string(`member.status.${theOtherOne.statusName}`)}]</span> : null}
             {chat.public && <div className="hint--bottom" data-hint={Lang.string('chat.public.label')}><Icon className="text-green" name="access-point" /></div>}
             {chat.mute && <div className="hint--bottom" data-hint={Lang.string('chat.mute.label')}><Icon className="text-brown" name="bell-off" /></div>}
             {chat.isDismissed && <div className="small label rounded dark">{Lang.string('chat.group.dismissed')}</div>}
