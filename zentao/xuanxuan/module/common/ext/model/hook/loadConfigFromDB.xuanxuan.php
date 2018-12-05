fa7c8f1fe6af715b9b7f5cd55438811aa8f4663b
chenfeiCF <chenfei@cnezsoft.com>
2018-12-05 10:40:00 +0800

    * Move zentao extension to zentaopms.

--- zentao/xuanxuan/module/common/ext/model/hook/loadConfigFromDB.xuanxuan.php
+++
@@ -1,3 +0,0 @@
-<?php
-$sn = $this->loadModel('setting')->getItem('owner=system&module=xuanxuan&key=key');
-if(empty($sn)) $this->setting->setItem('system.xuanxuan..key', $this->setting->computeSN());
