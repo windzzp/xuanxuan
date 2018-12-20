6a35293ce82bf37de3e9e30da6ef697331fa33a8
Catouse <catouse@gmail.com>
2018-12-20 11:09:17 +0800

    * change dev tools window default position.

--- xxc/app/platform/electron/app-remote.js
+++ xxc/app/platform/electron/app-remote.js
@@ -146,7 +146,7 @@ class AppRemote {
                 Lang.update(langInConfig);
             }
             this.createTrayIcon(windowName);
-            if (SHOW_LOG) console.log('\n>> App ready.', config);
+            if (SHOW_LOG) console.log('\n>> App ready.');
         });
 
         // 设置 Electron 应用标题
@@ -193,10 +193,12 @@ class AppRemote {
      */
     removeTrayIcon(windowName) {
         if (this._traysData && this._traysData[windowName]) {
-            const {tray} = this._traysData[windowName];
+            const trayData = this._traysData[windowName];
+            const {tray} = trayData;
             if (tray) {
                 tray.destroy();
             }
+            trayData.tray = null;
             delete this._traysData[windowName];
         }
     }
@@ -296,7 +298,7 @@ class AppRemote {
             hashRoute: '/index',
             name: windowName,
             resizable: true,
-            debug: true
+            debug: DEBUG
         }, options);
 
         if (DEBUG && !hasMainWindow) {
@@ -387,6 +389,10 @@ class AppRemote {
             delete windowSetting[optionName];
         });
         browserWindow = new BrowserWindow(windowSetting);
+        if (DEBUG) {
+            console.log(`>> Create window "${name}" with setting: `, windowSetting);
+        }
+
         this.windows[name] = browserWindow;
         browserWindow.on('closed', () => {
             delete this.windows[name];
@@ -438,7 +444,7 @@ class AppRemote {
         }
 
         if (options.debug && DEBUG) {
-            browserWindow.openDevTools();
+            browserWindow.webContents.openDevTools({mode: 'bottom'});
             browserWindow.webContents.on('context-menu', (e, props) => {
                 const {x, y} = props;
                 Menu.buildFromTemplate([{
@@ -449,23 +455,21 @@ class AppRemote {
                 }]).popup(browserWindow);
             });
 
-            if (DEBUG) {
-                browserWindow.webContents.on('crashed', () => {
-                    const messageBoxOptions = {
-                        type: 'info',
-                        title: 'Renderer process crashed.',
-                        message: 'The renderer process has been crashed, you can reload or close it.',
-                        buttons: ['Reload', 'Close']
-                    };
-                    dialog.showMessageBox(messageBoxOptions, (index) => {
-                        if (index === 0) {
-                            browserWindow.reload();
-                        } else {
-                            browserWindow.close();
-                        }
-                    });
+            browserWindow.webContents.on('crashed', () => {
+                const messageBoxOptions = {
+                    type: 'info',
+                    title: 'Renderer process crashed.',
+                    message: 'The renderer process has been crashed, you can reload or close it.',
+                    buttons: ['Reload', 'Close']
+                };
+                dialog.showMessageBox(messageBoxOptions, (index) => {
+                    if (index === 0) {
+                        browserWindow.reload();
+                    } else {
+                        browserWindow.close();
+                    }
                 });
-            }
+            });
         }
 
         return browserWindow;
@@ -625,7 +629,9 @@ class AppRemote {
         if (flash) {
             if (!trayData.flashTask) {
                 trayData.flashTask = setInterval(() => {
-                    trayData.tray.setImage(this._trayIcons[(trayData.iconCounter++) % 2]);
+                    if (trayData.tray) {
+                        trayData.tray.setImage(this._trayIcons[(trayData.iconCounter++) % 2]);
+                    }
                 }, 400);
             }
         } else {
