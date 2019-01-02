0c7e62a5328c5223be4c456ccc179ed942dd20cd
zbx <472455205@qq.com>
2019-01-02 15:30:21 +0800

    * Fix chat-send-code.

--- xxc/app/views/chats/chat-send-code-dialog.js
+++ xxc/app/views/chats/chat-send-code-dialog.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Modal from '../../components/modal';
-<<<<<<< HEAD:xxc/app/views/chats/chat-code-dialog.js
-import ChatCode from './chat-code';
-import Lang from '../../core/lang';
-import { isAbsolute } from 'path';
-import LangHelper from '../../utils/lang-helper';
-=======
 import ChatCode from './chat-send-code';
 import Lang from '../../core/lang';
+import App from '../../core';
 
->>>>>>> 79ea1ca634c397b97b1c7f645b1d6b8fb74c3f6a:xxc/app/views/chats/chat-send-code-dialog.js
+/**
+ * 处理发送代码按钮点击事件
+ * @param {string} language 语言类型
+ * @param {string} code 代码
+ * @param {Chat} chat 聊天实例
+ * @memberof ChatSendCodeDialog
+ * @private
+ * @return {void}
+ */
+const handleSubmitBtnClick = async (language, code, chat) => {
+    const codeLanguage = language.toLowerCase();
+    const codeContent = `\`\`\`${codeLanguage}\n${code}\n\`\`\``;
+
+    if (code === '') {
+        return;
+    }
+
+    await App.im.server.sendTextMessage(codeContent, chat, true); // eslint-disable-line
+    App.im.ui.activeChat(chat, 'recents');
+};
+
 /**
  * 显示发送代码对话框界面
  * @param {Chat} chat 聊天对象
@@ -18,15 +33,13 @@ import Lang from '../../core/lang';
  */
 export const showChatCodeDialog = (chat, callback) => {
     const modalId = 'app-chat-code-dialog';
-    let chatCode = '';
+    let chatSendCode = null;
     return Modal.show({
         id: modalId,
         title: Lang.string('chat.sendCodeDialog.title'),
         style: {
             width: 600,
-            height: 430,
-            left: '50%',
-            marginLeft: -300,
+            height: 450,
             position: 'absolute',
             bottom: 0,
         },
@@ -35,16 +48,17 @@ export const showChatCodeDialog = (chat, callback) => {
         actions: [
             {
                 type: 'submit',
-                label: Lang.string('chat.sendbox.toolbar.code'),
+                label: Lang.string('chat.sendCode.sendBtnLabel'),
                 click: () => {
-                    chatCode.handleSubmitBtnClick();
+                    const codeInfo = chatSendCode.getCode();
+                    handleSubmitBtnClick(codeInfo.language, codeInfo.code, chat);
                 }
             },
             {
                 type: 'cancel',
             }
         ],
-        content: <ChatCode ref={e => {chatCode = e;}} chat={chat} onRequestClose={() => (Modal.hide(modalId))} />
+        content: <ChatCode ref={e => {chatSendCode = e;}} chat={chat} />
     }, callback);
 };
 
