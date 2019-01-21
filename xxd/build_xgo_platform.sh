25c9f2d505e1439f13a234cde4d20cf698646569
滔哥 <admin@lvtao.net>
2019-01-21 09:31:14 +0800

    * Fix bug.

--- xxd/build_xgo_platform.sh
+++ xxd/build_xgo_platform.sh
@@ -12,7 +12,7 @@ folder=$version.mac
 if [ ! -d $folder ]; then
     mkdir $folder
 fi
-xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 -ldflags '-extldflags "-static"' --targets=darwin/amd64 .
+xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 --targets=darwin/amd64 .
 mv $folder/xxd* $folder/xxd
 cp -Ra config $folder
 tar zcf $folder.tar.gz $folder
@@ -48,7 +48,7 @@ if [ ! -d $folder ]; then
     mkdir $folder
 fi
 echo "build win64 ..."
-xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 -ldflags '-extldflags "-static"' --targets=windows/amd64 .
+xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 --targets=windows/amd64 .
 mv $folder/xxd* $folder/xxd.exe
 cp -Ra config $folder
 zip -rq $folder.zip $folder
@@ -60,9 +60,9 @@ if [ ! -d $folder ]; then
     mkdir $folder
 fi
 echo "build win32 ..."
-xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 -ldflags '-extldflags "-static"' --targets=windows/386 .
+xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 --targets=windows/386 .
 mv $folder/xxd* $folder/xxd.exe
-cp -ra config $folder
+cp -Ra config $folder
 zip -rq $folder.zip $folder
 rm -rf $folder
 
