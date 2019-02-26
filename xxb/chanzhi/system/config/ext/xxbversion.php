ca29e23ca0dae5427e705f2c57ef839fbbaf1483
滔哥 <admin@lvtao.net>
2019-02-26 17:11:19 +0800

    * Fix bug.

--- xxb/chanzhi/system/config/ext/xxbversion.php
+++
@@ -1,4 +0,0 @@
-<?php
-$config->menus->content .= ',xxbversion';
-$config->rights->guest['xxbversion']['index']  = 'index';
-if(!defined('TABLE_XXB_VERSION')) define('TABLE_XXB_VERSION', '`' . $config->db->prefix . 'xxb_version`');
