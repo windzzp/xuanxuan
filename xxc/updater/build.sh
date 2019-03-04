a738b014a12ee8ccb383facb1cf8666ab6397f42
滔哥 <admin@lvtao.net>
2019-03-04 10:41:39 +0800

    * Change updater build file name.

--- xxc/updater/build.sh
+++ xxc/updater/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o updater_mac main.go
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o updater_linux_x64 main.go
-CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o updater_linux_i386 main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o updater_win_x64.exe main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o updater_win_i386.exe main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./bin/updater.mac main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/updater.linux64 main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o ./bin/updater.linux32 main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./bin/updater.win64.exe main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o ./bin/updater.win32.exe main.go
 
 echo "build end "
 
