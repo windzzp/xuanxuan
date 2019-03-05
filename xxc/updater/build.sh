73e3a7d382472928cf23bee2ea3bfb700daca050
滔哥 <admin@lvtao.net>
2019-03-05 08:35:45 +0800

    * Add manifest for windows.

--- xxc/updater/build.sh
+++ xxc/updater/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+rsrc -manifest updater.manifest -o updater.syso
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./bin/updater.mac main.go
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/updater.linux64 main.go
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o ./bin/updater.linux32 main.go
