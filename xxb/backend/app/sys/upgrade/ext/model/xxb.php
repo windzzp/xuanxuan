0298045f5accaacbedb7ba1e99a5f9e07ff874f2
滔哥 <admin@lvtao.net>
2019-03-11 11:36:49 +0800

    * Fix bug.

--- xxb/backend/app/sys/upgrade/ext/model/xxb.php
+++ xxb/backend/app/sys/upgrade/ext/model/xxb.php
@@ -9,6 +9,10 @@ public function execute($fromVersion)
         case '1_2'   : $this->execSQL($this->getUpgradeFile('upgradexxb1.2.sql'));
         case '2_0_0' :
         case '2_1_0' :
+        case '2_2_0' :
+        case '2_3_0' :
+        case '2_4_0' :
+        case '2_4_1' :
         default : $this->loadModel('setting')->updateVersion($this->config->version);
     }
 }
