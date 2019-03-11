0b7c172d9f17902bfffca9f0c888ac8be9795d2e
滔哥 <admin@lvtao.net>
2019-03-11 17:27:26 +0800

    * Fix bug.

--- ranzhi/db/upgradexuanxuan2.4.0.sql
+++ ranzhi/db/upgradexuanxuan2.4.0.sql
@@ -1,5 +1,4 @@
 ALTER TABLE `im_messagestatus` ADD `message` INT(11)  UNSIGNED  NOT NULL  AFTER `user`;
-ALTER TABLE `sys_category` ADD `deleted` enum('0', '1') NOT NULL DEFAULT '0';
 
 CREATE TABLE IF NOT EXISTS `im_xxcversion` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
---
+++ xxb/backend/db/upgradexxb2.4.0.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `sys_category` ADD `deleted` enum('0', '1') NOT NULL DEFAULT '0';
