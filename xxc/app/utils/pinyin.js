f815914b1a3a7a30ef2136a084f03e84a778e09a
Catouse <catouse@gmail.com>
2019-03-13 10:40:22 +0800

    * finish task #312 and fix bug #56.

--- xxc/app/utils/pinyin.js
+++ xxc/app/utils/pinyin.js
@@ -1,41 +1,18 @@
-import PinYin from 'pinyin';
-
-/** @module pinyin */
+import PinYin from 'tiny-pinyin';
 
 /**
  * 将文本字符串中的中文转换为拼音形式
  * @param  {string} str 要转换的字符串
- * @param  {string|string[]} [styles='default'] 样式类型，包括 'STYLE_NORMAL'，'STYLE_FIRST_LETTER'，'STYLE_INITIALS'
  * @param  {string} [separator=' '] 拼音分隔符
- * @return {string}
+ * @return {string} 转换后的拼音字符串
  * @function
  * @see https://github.com/hotoo/pinyin
  */
-export default (str, styles = 'default', separator = ' ') => {
-    if (!styles || styles === 'default') {
-        styles = [PinYin.STYLE_NORMAL, PinYin.STYLE_FIRST_LETTER, PinYin.STYLE_INITIALS];
+export default (str, separator = ' ') => {
+    if (typeof str === 'string' && str.length) {
+        const pinyins = PinYin.parse(str);
+        const targetStrings = pinyins.map(x => x.target);
+        return `${targetStrings.join('')}${separator}${targetStrings.map(x => (x.length ? x[0] : '')).join('')}`.toLowerCase();
     }
-    if (!Array.isArray(styles)) {
-        styles = [styles];
-    }
-    const pinyins = styles.map(style => {
-        if (typeof style === 'string') {
-            switch (style) {
-            case 'normal':
-            case 'STYLE_NORMAL':
-                style = PinYin.STYLE_NORMAL;
-                break;
-            case 'first-letter':
-            case 'STYLE_FIRST_LETTER':
-                style = PinYin.STYLE_FIRST_LETTER;
-                break;
-            case 'initials':
-            case 'STYLE_INITIALS':
-                style = PinYin.STYLE_INITIALS;
-                break;
-            }
-        }
-        return PinYin(str, {style}).map(x => x[0]).join('');
-    });
-    return separator === false ? pinyins : pinyins.join(separator);
+    return '';
 };
