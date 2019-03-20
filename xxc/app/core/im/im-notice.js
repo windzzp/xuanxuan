9ca9fbd851333259c9a3ba102f49c70e094a33ed
Lwrless <happy.lwr@gmail.com>
2019-03-20 17:34:41 +0800

    * fix emoji not showing up in notifications.

--- xxc/app/core/im/im-notice.js
+++ xxc/app/core/im/im-notice.js
@@ -39,7 +39,9 @@ const getPlainTextOfChatMessage = (chatMessage, limitLength = 255, ignoreBreak =
     if (chatMessage.isObjectContent && chatMessage.objectContentType === 'url') {
         plainText = chatMessage.objectContent.url;
     } else {
-        plainText = chatMessage.renderedTextContent(renderChatMessageContent).replace(/<(?:.|\n)*?>/gm, '');
+        plainText = chatMessage.renderedTextContent(renderChatMessageContent).replace(/<img class="emojione" alt="/gm, '');
+        plainText = plainText.replace(/" title=":[a-z\_]*:" src="media\/emojione\/png\/[0-9a-z]*.png"\/>/gm, '');
+        plainText = plainText.replace(/<(?:.|\n)*?>/gm, '');
     }
     if (ignoreBreak) {
         plainText = plainText.trim().replace(/[\r\n]/g, ' ').replace(/\n[\s| | ]*\r/g, '\n');
