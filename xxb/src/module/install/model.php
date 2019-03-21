e41a41b1a53664bc5e330db71b966cbde2e4bd61
滔哥 <admin@lvtao.net>
2019-03-21 15:10:19 +0800

    * Fix bug.

--- xxb/src/module/install/model.php
+++ xxb/src/module/install/model.php
@@ -349,6 +349,7 @@ class installModel extends model
             {
                 $table = str_replace('--', '', $table);
             }
+            $table = str_replace('`im_', '`' . $this->config->db->prefix . 'im_', $table);
             $table = str_replace('`xxb_', $this->config->db->name . '.`xxb_', $table);
             $table = str_replace('xxb_', $this->config->db->prefix, $table);
             if(!$this->dbh->query($table)) return false;
