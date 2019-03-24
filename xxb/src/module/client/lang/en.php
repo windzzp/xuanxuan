acdfb1de05cd1673b800b6323caf9acd8c4a81cd
滔哥 <admin@lvtao.net>
2019-03-25 01:29:33 +0800

    * Task#319.

---
+++ xxb/src/module/client/lang/en.php
@@ -0,0 +1,33 @@
+<?php
+$lang->client->common       = 'Client';
+$lang->client->browse       = 'Browse client';
+$lang->client->edit         = 'Edit client';
+$lang->client->delete       = 'Delete client';
+$lang->client->checkUpgrade = 'Check upgrade';
+
+$lang->client->id              = 'ID';
+$lang->client->version         = 'Version';
+$lang->client->xxcVersion      = 'XXC Version';
+$lang->client->desc            = 'Description';
+$lang->client->changeLog       = 'Log';
+$lang->client->strategy        = 'Strategy';
+$lang->client->download        = 'Download';
+$lang->client->downloading     = 'Downloading...';
+$lang->client->downloadLink    = 'Download link';
+$lang->client->downloadFail    = 'Download fail';
+$lang->client->downloadSuccess = 'Download success';
+$lang->client->releaseStatus   = 'Status';
+$lang->client->wrongVersion    = '<strong>Version</strong> format is incorrect. Example：2.5 or 2.5.0';
+$lang->client->downloadTip     = 'Please check the integrity of the package after downloading. If it is damaged, please upload it to the corresponding directory of www/data/client.';
+
+$lang->client->strategies['force']    = 'Force';
+$lang->client->strategies['optional'] = 'Optional';
+
+$lang->client->status['release']    = 'Release';
+$lang->client->status['notRelease'] = 'Not release';
+
+$lang->client->zipList['win64zip']   = 'Windows 64';
+$lang->client->zipList['win32zip']   = 'Windows 32';
+$lang->client->zipList['macOS']      = 'macOS';
+$lang->client->zipList['linux64zip'] = 'Linux 64';
+$lang->client->zipList['linux32zip'] = 'Linux 32';
