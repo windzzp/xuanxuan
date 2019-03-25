8933d7b2126b28add0096fe5d0f7a28123bce035
滔哥 <admin@lvtao.net>
2019-03-25 17:42:05 +0800

    * task#337.

--- xxb/src/www/ux.php
+++ xxb/src/www/ux.php
@@ -1,12 +1,25 @@
 <?php
-/* Set the error reporting. */
-error_reporting(E_ALL);
-
-/* Start output buffer. */
-ob_start();
-
-/* Define the run mode as front. */
+/**
+ * The upgrade router file of XXB.
+ *
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL (http://zpl.pub/page/zplv12.html)
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     XXB
+ * @version     $Id: upgrade.php 3138 2015-11-09 07:32:18Z chujilu $
+ * @link        http://xuan.im
+ */
+/* Judge my.php exists or not. */
 define('RUN_MODE', 'upgrade');
+$myConfig = dirname(dirname(dirname(__FILE__))) . '/config/my.php';
+if(!file_exists($myConfig))
+{
+    echo "文件" . $myConfig . "不存在！ 提示：不要重命名原来的喧喧安装目录，下载最新的源码包，覆盖即可。" . "<br />";
+    echo $myConfig . " doesn't exists! Please don't rename xxb before overriding the source code!";
+    exit;
+}
+
+error_reporting(0);
 
 /* Load the framework. */
 include '../framework/router.class.php';
@@ -14,13 +27,9 @@ include '../framework/control.class.php';
 include '../framework/model.class.php';
 include '../framework/helper.class.php';
 
-/* Log the time and define the run mode. */
-$startTime = getTime();
-
-/* Run the app. */
-$appName = 'sys';
-$app     = router::createApp($appName);
-$common  = $app->loadCommon();
+/* Instance the app. */
+$app = router::createApp('xxb', dirname(dirname(__FILE__)));
+$common = $app->loadCommon();
 
 /* Reset the config params to make sure the upgrade program will be lauched. */
 $config->set('requestType',    'GET');
