eeb92d3ebcd122fcccac090c2cf4166ace8728e7
liugang <liugang@cnezsoft.com>
2019-03-26 17:06:25 +0800

    * Change VERSION. * Adjust lang files.

---
+++ xxb/src/module/client/lang/zh-tw.php
@@ -0,0 +1,34 @@
+<?php
+$lang->client->common       = '客戶端';
+$lang->client->create       = '添加客戶端';
+$lang->client->browse       = '瀏覽客戶端';
+$lang->client->edit         = '編輯客戶端';
+$lang->client->delete       = '刪除客戶端';
+$lang->client->checkUpgrade = '檢查更新';
+
+$lang->client->id              = 'ID';
+$lang->client->version         = '版本';
+$lang->client->xxcVersion      = 'XXC版本';
+$lang->client->desc            = '升級描述';
+$lang->client->changeLog       = '更新日誌';
+$lang->client->strategy        = '升級策略';
+$lang->client->download        = '下載';
+$lang->client->downloading     = '下載中...';
+$lang->client->downloadLink    = '下載地址';
+$lang->client->downloadFail    = '下載失敗';
+$lang->client->downloadSuccess = '下載成功';
+$lang->client->releaseStatus   = '發佈狀態';
+$lang->client->wrongVersion    = '<strong>版本</strong>格式不正確，例如：2.5 或者 2.5.0';
+$lang->client->downloadTip     = '下載後請檢查壓縮包完整性，如果壓縮包損壞，請使用其它工具下載後上傳至www/data/client對應的目錄下';
+
+$lang->client->strategies['force']    = '強制';
+$lang->client->strategies['optional'] = '可選';
+
+$lang->client->status['wait']     = '待發佈';
+$lang->client->status['released'] = '已發佈';
+
+$lang->client->zipList['win64zip']   = 'Windows 64位';
+$lang->client->zipList['win32zip']   = 'Windows 32位';
+$lang->client->zipList['macOS']      = 'macOS';
+$lang->client->zipList['linux64zip'] = 'Linux 64位';
+$lang->client->zipList['linux32zip'] = 'Linux 32位';
--- xxb/src/module/common/lang/zh-tw.php
+++ xxb/src/module/common/lang/zh-tw.php
@@ -148,14 +148,13 @@ EOT;
 
 /* Items for javascript. */
 $lang->js = new stdclass();
-$lang->js->confirmDelete         = '您確定要執行刪除操作嗎？';
-$lang->js->confirmFinish         = '您確定要執行完成操作嗎？';
-$lang->js->deleteing             = '刪除中';
-$lang->js->doing                 = '處理中';
-$lang->js->timeout               = '網絡超時,請重試';
-$lang->js->confirmDiscardChanges = '表單已更改，確定關閉？';
-$lang->js->yes                   = '是';
-$lang->js->no                    = '否';
+$lang->js->confirmDelete = '您確定要執行刪除操作嗎？';
+$lang->js->confirmFinish = '您確定要執行完成操作嗎？';
+$lang->js->deleteing     = '刪除中';
+$lang->js->doing         = '處理中';
+$lang->js->timeout       = '網絡超時,請重試';
+$lang->js->yes           = '是';
+$lang->js->no            = '否';
 
 /* The main menus. */
 $lang->menu = new stdclass();
@@ -164,57 +163,25 @@ $lang->menu->group   = '權限|group|browse|';
 $lang->menu->entry   = '應用|entry|admin|';
 $lang->menu->setting = '設置|setting|xuanxuan|';
 
-$lang->index   = new stdclass();
-$lang->user    = new stdclass();
-$lang->file    = new stdclass();
-$lang->tree    = new stdclass();
-$lang->mail    = new stdclass();
-$lang->dept    = new stdclass();
-$lang->block   = new stdclass();
-$lang->action  = new stdclass();
-$lang->setting = new stdclass();
-
-$lang->group   = new stdclass();
-
-$lang->menuGroups = new stdclass();
-
-/* Menu entry. */
+/* Entry menu. */
 $lang->entry       = new stdclass();
 $lang->entry->menu = new stdclass();
 $lang->entry->menu->admin    = array('link' => '應用列表|entry|admin|', 'alias' => 'edit, integration, style, zentaoAdmin');
 $lang->entry->menu->create   = '添加應用|entry|create|';
 $lang->entry->menu->category = '分組|entry|category|';
 
-/* Menu system. */
-$lang->system       = new stdclass();
-$lang->system->menu = new stdclass();
-$lang->system->menu->mail   = array('link' => '發信|mail|admin|', 'alias' => 'detect,edit,save,test');
-$lang->system->menu->trash  = array('link' => '資源回收筒|action|trash|');
-$lang->system->menu->cron   = '計劃任務|cron|index|';
-$lang->system->menu->backup = '備份|backup|index|';
+/* Setting menu. */
+$lang->setting       = new stdclass();
+$lang->setting->menu = new stdClass();
+$lang->setting->menu->xuanxuan = '喧喧|setting|xuanxuan|';
+$lang->setting->menu->client   = '客戶端|client|browse|';
 
-/* Menu of mail module. */
-$lang->mail = new stdclass();
-$lang->mail->menu = $lang->system->menu;
-$lang->menuGroups->mail = 'system';
+$lang->client = new stdclass();
+$lang->client->menu = $lang->setting->menu;
 
-/* Menu of action module. */
-$lang->action = new stdclass();
-$lang->action->menu = $lang->system->menu;
-$lang->menuGroups->action = 'system';
-
-/* Menu of cron module. */
-$lang->cron = new stdclass();
-$lang->cron->menu = $lang->system->menu;
-$lang->menuGroups->cron = 'system';
-
-/* Menu of backup module. */
-$lang->backup = new stdclass();
-$lang->backup->menu = $lang->system->menu;
-$lang->menuGroups->backup = 'system';
-
-$lang->setting->menu = $lang->system->menu;
-$lang->menuGroups->setting = 'system';
+$lang->menuGroups = new stdclass();
+$lang->menuGroups->tree   = 'user';
+$lang->menuGroups->client = 'setting';
 
 /* The error messages. */
 $lang->error = new stdclass();
