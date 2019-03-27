3e80021ac16d0cc4f08bf2e29915ce0322468842
滔哥 <admin@lvtao.net>
2019-03-27 14:45:41 +0800

    * Fix bug.

--- xxb/src/db/upgrade2.5.0.sql
+++ xxb/src/db/upgrade2.5.0.sql
@@ -1,3 +1,14 @@
+ALTER TABLE `xxb_block` DROP INDEX `accountAppOrder`;
+ALTER TABLE `xxb_block` DROP INDEX `account`;
+ALTER TABLE `xxb_config` DROP INDEX `unique`;
+ALTER TABLE `xxb_lang` DROP INDEX `lang`;
+
 ALTER TABLE `xxb_block` DROP `app`;
 ALTER TABLE `xxb_config` DROP `app`;
 ALTER TABLE `xxb_lang` DROP `app`;
+
+ALTER TABLE `xxb_block` ADD UNIQUE INDEX `accountAppOrder` (`account`, `order`);
+ALTER TABLE `xxb_block` ADD INDEX `account` (`account`);
+ALTER TABLE `xxb_config` ADD UNIQUE INDEX `unique` (`owner`, `module`, `section`, `key`);
+ALTER TABLE `xxb_lang` ADD INDEX `lang` (`lang`, `module`, `section`, `key`);
+
