8f0abd2540b8719ed2db98feb7d7492992993b51
滔哥 <admin@lvtao.net>
2019-03-28 13:47:13 +0800

    * Fix bug.

--- xxb/src/module/upgrade/config.php
+++ xxb/src/module/upgrade/config.php
@@ -5,3 +5,7 @@ $config->delete['2.5.0'][] = 'app';
 $config->delete['2.5.0'][] = 'www/sys';
 $config->delete['2.5.0'][] = 'config/ext/xxb.php';
 $config->delete['2.5.0'][] = 'framework/loader.php';
+$config->delete['2.5.0'][] = 'tmp/model/common.php';
+$config->delete['2.5.0'][] = 'tmp/model/install.php';
+$config->delete['2.5.0'][] = 'tmp/model/upgrade.php';
+$config->delete['2.5.0'][] = 'tmp/model/user.php';
