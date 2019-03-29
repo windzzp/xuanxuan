698bff8faa73f38227ea1bc558dcaee72cd76053
liugang <liugang@cnezsoft.com>
2019-03-29 16:47:59 +0800

    * Fix bug of menu.

--- ranzhi/app/sys/common/ext/lang/zh-cn/xuanxuan.php
+++ ranzhi/app/sys/common/ext/lang/zh-cn/xuanxuan.php
@@ -6,6 +6,7 @@ $lang->system->menuOrder[4] = 'xuanxuan';
 $lang->system->menuOrder[5] = 'client';
 
 $lang->client = new stdclass();
-$lang->client->menu = $lang->system->menu;
+$lang->client->menu      = $lang->system->menu;
+$lang->client->menuOrder = $lang->system->menuOrder;
 
 $lang->menuGroups->client = 'system';
